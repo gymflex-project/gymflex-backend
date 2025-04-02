c2f9379c2255049cb0c9ee6825b61bf5860f9b19
Pedro Andrade <pedrogamerp@gmail.com>
2025-04-01 23:54:53 -0300

    GM-37 feat(repository): improve with Unit of Work pattern for better transaction management

---
+++ src/GymFlex.Infrastructure/Data/UnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using GymFlex.Application.Interfaces;
+using GymFlex.Infrastructure.Data.Context;
+
+namespace GymFlex.Infrastructure.Data
+{
+    public class UnitOfWork(ApplicationDbContext context) : IUnitOfWork
+    {
+        public Task Commit(CancellationToken cancellationToken)
+            => context.SaveChangesAsync(cancellationToken);
+
+        public Task Rollback(CancellationToken cancellationToken)
+            => Task.CompletedTask;
+    }
+}
